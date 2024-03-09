0080cb6aac05feaa95118d4b60daa6ef7b99218b
Teja1004 <dev4teja@gmail.com>
2024-03-09 16:53:39 +0530

    dummy json server setup

--- src/app/test.service.ts
+++ src/app/test.service.ts
@@ -19,7 +19,7 @@ export class TestService {
   }
 
   getData() {
-    return this.http.get(`https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8`);
+    return this.http.get(`http://localhost:3000/users`);
   }
 
 }
